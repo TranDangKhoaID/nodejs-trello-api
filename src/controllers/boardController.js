edf3a1b118443385382bc333980f2772008eaa73
khoa.trandang2 <khoa.trandang2@di4l.vn>
2025-01-24 10:44:08 +0700

    error handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -2,21 +2,17 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log(req.body)
-    console.log(req.query)
-    console.log(req.params)
-    console.log(req.files)
-    console.log(req.coockie)
-    console.log(req.jwdDecoded)
+    // console.log(req.body)
+    // console.log(req.query)
+    // console.log(req.params)
+    // console.log(req.files)
+    // console.log(req.coockie)
+    // console.log(req.jwdDecoded)
 
     //Điều hướng dữ liệu sang tầng Service
-
+    //throw new ApiError(StatusCodes.BAD_GATEWAY, 'trankhoa2610 test error')
     res.status(StatusCodes.CREATED).json({ message: 'POST from controller: API create new board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
