5f79efabd94bfbe401a13d733d0a01a3af586ae9
khoa.trandang2 <khoa.trandang2@di4l.vn>
2025-03-11 10:07:11 +0700

    update array columnOrderIds and cardOrderIds

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -1,3 +1,4 @@
+import { columnModel } from '~/models/columnModel'
 import { cardModel } from '~/models/cardModel'
 
 const createNew = async (reqBody) => {
@@ -8,7 +9,12 @@ const createNew = async (reqBody) => {
     }
     const createdCard = await cardModel.createNew(newCard)
     const getNewCard = await cardModel.findOneById(createdCard.insertedId)
-    //...
+
+    if (getNewCard) {
+      //
+      await columnModel.pushCardOrderIds(getNewCard)
+    }
+
     return getNewCard
   } catch (error) { throw error }
 }
--- src/services/columnService.js
+++ src/services/columnService.js
@@ -1,4 +1,5 @@
 import { columnModel } from '~/models/columnModel'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   try {
@@ -8,7 +9,13 @@ const createNew = async (reqBody) => {
     }
     const createdColumn = await columnModel.createNew(newColumn)
     const getNewColumn = await columnModel.findOneById(createdColumn.insertedId)
-    //...
+
+    if (getNewColumn) {
+      getNewColumn.cards = []
+      //cap nhat lai columnOrderIds
+      await boardModel.pushColumnOrderIds(getNewColumn)
+    }
+
     return getNewColumn
   } catch (error) { throw error }
 }
